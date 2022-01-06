b44fa36b88868f38a648d2e586580f9573aff913
hawkinchina <free_hawk@sina.com>
2022-01-06 13:48:23 +0800

    1 file modified

--- src/app/module/product/product-routing.module.ts
+++ src/app/module/product/product-routing.module.ts
@@ -4,12 +4,25 @@ import { ProductComponent } from './product.component';
 import { ListComponent } from './components/list/list.component';
 import { PriceComponent } from './components/price/price.component';
 
-const routes: Routes = [
-  { path: '', component: ProductComponent },
-  { path: 'list', component: ListComponent },
-  { path: 'price', component: PriceComponent },
-];
+//01.和子模块的根组件，配置成兄弟路由
+// const routes: Routes = [
+//   { path: '', component: ProductComponent },
 
+//   { path: 'list', component: ListComponent },
+//   { path: 'price', component: PriceComponent },
+// ];
+
+//02.和子模块的根组件，配置成父子路由
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductComponent,
+    children: [
+      { path: 'list', component: ListComponent },
+      { path: 'price', component: PriceComponent },
+    ],
+  },
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
