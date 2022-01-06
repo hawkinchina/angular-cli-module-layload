3691d7433aa602332a08da7bf6fcc1dc0a348803
hawkinchina <free_hawk@sina.com>
2022-01-06 12:20:17 +0800

    1 file modified

--- src/app/module/product/product-routing.module.ts
+++ src/app/module/product/product-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product.component';
 import { ListComponent } from './components/list/list.component';
 import { PriceComponent } from './components/price/price.component';
+
 const routes: Routes = [
   { path: '', component: ProductComponent },
   { path: 'list', component: ListComponent },
