1ffc6883382bca6f102cce5908a24ffe9929a989
hawkinchina <free_hawk@sina.com>
2022-01-06 12:04:43 +0800

    1 file modified

--- src/app/module/product/product-routing.module.ts
+++ src/app/module/product/product-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product.component';
-
-const routes: Routes = [{ path: '', component: ProductComponent }];
+import { ListComponent } from './list/list.component';
+import { PriceComponent } from './list/price.component';
+const routes: Routes = [
+  { path: '', component: ProductComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'price', component: PriceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
